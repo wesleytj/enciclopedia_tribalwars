a87cfe067c359e96e3920f6bd4ab7ac5390f1c72
USB - ANSR - CAE - Wesley Jacques <wesley.jacques@usb.educadventista.org>
2025-06-24 22:26:30 -0300

    Iniciando projeto com React e Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-yellow-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-800">
+        Enciclopédia Tribal Wars
+      </h1>
     </div>
   );
 }
